c186c0772dba30672212d223ab258649610f7a2b
bergmannlucas <lucaasberg@gmail.com>
2024-11-22 21:01:59 -0300

    Remove Brewfile and use brew with Nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,20 +1,65 @@
 {
-  description = "Zenful nix-darwin system flake";
+  description = "Bergmann nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.nvim
+      nixpkgs.config.allowUnfree = true;
+      environment.systemPackages = [ 
+        pkgs.kitty
+        pkgs.tmux
+        pkgs.neovim
+        pkgs.stow
+        pkgs.ripgrep
+        pkgs.fzf
+        pkgs.obsidian
+        pkgs.gh
+        pkgs.k9s
+        pkgs.vscode
+        pkgs.yazi
+        pkgs.starship
+        pkgs.hugo
+        pkgs.btop
+        pkgs.htop
+        pkgs.stats
+        pkgs.zoom-us
+        pkgs.zsh
+        pkgs.wget
+        pkgs.slack
+        pkgs.rectangle
+        pkgs.brave
+        pkgs.dbeaver-bin
+        pkgs.spotify
+      ];
+      
+      fonts.packages = with pkgs; [
+        (nerdfonts.override {fonts = ["JetBrainsMono"];})
+      ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "zsh-autosuggestions"
+          "awscli-local"
         ];
+        casks = [
+          "docker"
+          "insomnia"
+          "another-redis-desktop-manager"
+          "todoist"
+        ];
+        taps = [
+        ];
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -37,7 +82,18 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#lucas-bergmann
     darwinConfigurations."air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "bergmannlucas";
+            autoMigrate = true;
+          };
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
