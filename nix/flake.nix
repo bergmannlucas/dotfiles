5271b8f195bca258f62ba937751de12afc844662
bergmannlucas <lucaasberg@gmail.com>
2024-11-28 00:15:15 -0300

    update nix darwin config

--- nix/flake.nix
+++ nix/flake.nix
@@ -10,11 +10,12 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       nixpkgs.config.allowUnfree = true;
       environment.systemPackages = [ 
+        pkgs.mkalias
         pkgs.kitty
         pkgs.zsh
         pkgs.oh-my-posh
@@ -36,7 +37,6 @@
         pkgs.wget
         pkgs.slack
         pkgs.rectangle
-        pkgs.brave
         pkgs.dbeaver-bin
         pkgs.spotify
       ];
@@ -50,6 +50,9 @@
         brews = [
           "mas"
           "awscli-local"
+          "pyenv"
+          "virtualenv"
+          "pyenv-virtualenv"
         ];
         casks = [
           "docker"
@@ -57,6 +60,7 @@
           "another-redis-desktop-manager"
           "todoist"
           "notion-calendar"
+          "brave-browser"
         ];
         taps = [
         ];
@@ -64,6 +68,26 @@
           Vetero = 6449091332;
         };
       };
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -79,7 +103,7 @@
           "${pkgs.obsidian}/Applications/Obsidian.app"
           "/Applications/Todoist.app"
           "${pkgs.slack}/Applications/Slack.app"
-          "${pkgs.brave}/Applications/Brave Browser.app"
+          "/Applications/Brave Browser.app"
           "${pkgs.kitty}/Applications/kitty.app"
           "${pkgs.vscode}/Applications/Visual Studio Code.app"
           "/Applications/Insomnia.app"
@@ -107,7 +131,7 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#lucas-bergmann
+    # $ darwin-rebuild build --flake .#air
     darwinConfigurations."air" = nix-darwin.lib.darwinSystem {
       modules = [ 
         configuration
