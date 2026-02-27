b79a82c53fa62cd66dc42d3b11df0b2f8f26f5e4
bergmannlucas <lucaasberg@gmail.com>
2026-02-26 22:58:13 -0300

    nvim: remove and add some plugins

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+        files = {
+          hidden = true,
+        },
+        grep = {
+          hidden = true,
+        },
+      },
+    },
+    scroll = {
+      enabled = false,
+    },
+  },
+}
